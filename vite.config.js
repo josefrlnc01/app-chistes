30a1dc15e61846a5c8454523801fd9d07272570c
josefrlnc01 <josefrlnc01@gmail.com>
2025-08-07 01:20:56 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+// Usa la variable de entorno VITE_BASE para definir el base, por defecto '/'
 export default defineConfig({
-  base:'/app-chistes/',
+  base: process.env.VITE_BASE || '/',
   plugins: [react()],
   server: {
     historyApiFallback: true,
