c6f7e53c17530fd7a282432243f37c50ec848229
josefrlnc01 <josefrlnc01@gmail.com>
2025-08-07 15:55:52 +0200

    new

--- src/components/flyer.jsx
+++ src/components/flyer.jsx
@@ -2,11 +2,12 @@ export default function Flyer({ joke, visible }) {
   return (
     <div
       id="customFlyer"
-      style={{ display: visible ? "block" : "none" }}
+      className="min-w-full min-h-screen flex items-center justify-center bg-gray-800 bg-opacity-75 fixed top-0 left-0 z-50"
+      style={{ display: visible ? "block" : "none" } }
     >
       <div className="flyer-container">
         <div className="flyer-header">
-          <h2 id="flyer-title">🎩</h2>
+          <h2 id="flyer-title" className="text-center">🎩</h2>
         </div>
         <div className="flyer-content">
           <div className="comment-place">
